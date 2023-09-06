7b3e31ce233fff58434dd47aa1de8bd50e151f9e
Yahya <yahyahussein91@gmail.com>
2023-09-05 20:13:51 -0700

    setting up application context

--- Spring-01-Core-IoC/src/main/java/com/yahya/stereotype_annotation/MyApp.java
+++ Spring-01-Core-IoC/src/main/java/com/yahya/stereotype_annotation/MyApp.java
@@ -1,8 +1,11 @@
 package com.yahya.stereotype_annotation;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 public class MyApp {
     public static void main(String[] args) {
 
-
+        ApplicationContext context = new AnnotationConfigApplicationContext(ConfigCourse.class);
     }
 }
