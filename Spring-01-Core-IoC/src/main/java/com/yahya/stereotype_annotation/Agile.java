fc36c2b2fb0fc09354d42dde642209f72d2e85f0
Yahya <yahyahussein91@gmail.com>
2023-09-05 20:10:52 -0700

    using stereo type annotations

---
+++ Spring-01-Core-IoC/src/main/java/com/yahya/stereotype_annotation/Agile.java
@@ -0,0 +1,7 @@
+package com.yahya.stereotype_annotation;
+
+public class Agile {
+    public void getTeachingHours(){
+        System.out.println("Total teaching hours: 15");
+    }
+}
---
+++ Spring-01-Core-IoC/src/main/java/com/yahya/stereotype_annotation/Java.java
@@ -0,0 +1,7 @@
+package com.yahya.stereotype_annotation;
+
+public class Java {
+    public void getTeachingHours(){
+        System.out.println("Total teaching hours: 250");
+    }
+}
