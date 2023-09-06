8e646e1d65e9a844371fd0431bcd1d720f7fb09a
Yahya <yahyahussein91@gmail.com>
2023-09-05 20:23:19 -0700

    added more hours to java course

--- Spring-01-Core-IoC/src/main/java/com/yahya/stereotype_annotation/Java.java
+++ Spring-01-Core-IoC/src/main/java/com/yahya/stereotype_annotation/Java.java
@@ -5,6 +5,6 @@ import org.springframework.stereotype.Component;
 @Component
 public class Java {
     public void getTeachingHours(){
-        System.out.println("Total teaching hours: 250");
+        System.out.println("Total teaching hours: 255");
     }
 }
