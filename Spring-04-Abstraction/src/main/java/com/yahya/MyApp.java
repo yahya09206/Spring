bdb7902ab8ebc97f7d05b89d58b1f8b85bc98428
Yahya <yahyahussein91@gmail.com>
2023-09-06 20:46:16 -0700

    created application container

--- Spring-04-Abstraction/src/main/java/com/yahya/MyApp.java
+++ Spring-04-Abstraction/src/main/java/com/yahya/MyApp.java
@@ -1,6 +1,9 @@
 package com.yahya;
 
+import com.yahya.config.ProjectConfig;
 import com.yahya.model.Comment;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MyApp {
     public static void main(String[] args) {
@@ -8,6 +11,8 @@ public class MyApp {
         Comment comment = new Comment();
         comment.setAuthor("Johnson");
         comment.setText("Spring Framework");
+
+        ApplicationContext context = new AnnotationConfigApplicationContext(ProjectConfig.class);
 
 
     }
