ce4541470b9a20ec75f41f76d3ac44da98f9099e
Yahya <yahyahussein91@gmail.com>
2023-09-06 21:38:49 -0700

    injected custom qualifier

--- Spring-04-Abstraction/src/main/java/com/yahya/proxy/CommentPushNotificationProxy.java
+++ Spring-04-Abstraction/src/main/java/com/yahya/proxy/CommentPushNotificationProxy.java
@@ -2,14 +2,14 @@ package com.yahya.proxy;
 
 import com.yahya.model.Comment;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 @Component
-@Qualifier("Email")
-public class EmailCommentNotificationProxy implements CommentNotificationProxy{
+@Qualifier("Push")
+public class CommentPushNotificationProxy implements CommentNotificationProxy{
+
     @Override
     public void sendComment(Comment comment){
-        System.out.println("Sending Notifications for comment " + comment.getText());
+        System.out.println("Sending push notificcation for comment " + comment.getText());
     }
 }
--- Spring-04-Abstraction/src/main/java/com/yahya/service/CommentService.java
+++ Spring-04-Abstraction/src/main/java/com/yahya/service/CommentService.java
@@ -3,6 +3,7 @@ package com.yahya.service;
 import com.yahya.model.Comment;
 import com.yahya.proxy.CommentNotificationProxy;
 import com.yahya.repository.CommentRepository;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -11,7 +12,7 @@ public class CommentService {
     private final CommentRepository commentRepository;
     private final CommentNotificationProxy commentNotificationProxy;
 
-    public CommentService(CommentRepository commentRepository, CommentNotificationProxy commentNotificationProxy) {
+    public CommentService(CommentRepository commentRepository, @Qualifier("Push") CommentNotificationProxy commentNotificationProxy) {
         this.commentRepository = commentRepository;
         this.commentNotificationProxy = commentNotificationProxy;
     }
