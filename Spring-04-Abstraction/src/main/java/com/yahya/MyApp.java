42caa3550963be968debf45c67cd583fa037c193
Yahya <yahyahussein91@gmail.com>
2023-09-06 20:45:24 -0700

    created new comment object

---
+++ Spring-04-Abstraction/src/main/java/com/yahya/MyApp.java
@@ -0,0 +1,14 @@
+package com.yahya;
+
+import com.yahya.model.Comment;
+
+public class MyApp {
+    public static void main(String[] args) {
+
+        Comment comment = new Comment();
+        comment.setAuthor("Johnson");
+        comment.setText("Spring Framework");
+
+
+    }
+}
