d74d5c74596dd43ede8c2b0371ed8cd06234263b
Yahya <yahyahussein91@gmail.com>
2023-09-10 16:47:15 -0700

    changed file names

--- Spring-06-BootSpring-Demo/src/main/java/com/yahya/spring06bootspringdemo/MyApplication.java
+++ Spring-06-BootSpring-Demo/src/main/java/com/yahya/spring06bootspringdemo/MyApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MyApplication {
 
 	public static void main(String[] args) {
+		// Container created
 		SpringApplication.run(MyApplication.class, args);
 	}
-
 }
