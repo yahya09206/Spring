a5228aac7cd870dcee496f4778248935a7b16eb7
Yahya <yahyahussein91@gmail.com>
2023-09-11 21:47:55 -0700

    updated html page with thyme text to reflect controller

--- Spring-08-Spring-MVC-Model/src/main/java/com/yahya/controller/StudentController.java
+++ Spring-08-Spring-MVC-Model/src/main/java/com/yahya/controller/StudentController.java
@@ -1,13 +1,17 @@
 package com.yahya.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class StudentController {
 
     @RequestMapping("/welcome")
-    public String homePage(){
+    public String homePage(Model model){
+
+        model.addAttribute("name", "Cydeo");
+        model.addAttribute("course", "MVC");
         return "student/welcome";
     }
 }
