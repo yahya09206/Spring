13e6d12f5437701a841179cbc741dbe3611ae7e2
Yahya <yahyahussein91@gmail.com>
2023-09-19 21:02:46 -0700

    setting up method for student class

---
+++ Spring-09-Spring-Thymeleaf/src/main/java/com/yahya/bootstrap/DataGenerator.java
@@ -0,0 +1,16 @@
+package com.yahya.bootstrap;
+
+import com.yahya.model.Student;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class DataGenerator {
+
+    public static List<Student> createStudent(){
+
+        List<Student> students = Arrays.asList(
+
+        )
+    }
+}
--- Spring-09-Spring-Thymeleaf/src/main/java/com/yahya/model/Student.java
+++ Spring-09-Spring-Thymeleaf/src/main/java/com/yahya/model/Student.java
@@ -5,4 +5,5 @@ public class Student {
     private String firstName;
     private String lastName;
     private int age;
+    private String state;
 }
