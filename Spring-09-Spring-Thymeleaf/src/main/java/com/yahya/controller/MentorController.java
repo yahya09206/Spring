9b7edfbe94c5477535a15ed2c82f4a01131e7f7a
Yahya <yahyahussein91@gmail.com>
2023-09-20 20:36:03 -0700

    imported postmapping class

--- Spring-09-Spring-Thymeleaf/src/main/java/com/yahya/controller/MentorController.java
+++ Spring-09-Spring-Thymeleaf/src/main/java/com/yahya/controller/MentorController.java
@@ -4,6 +4,7 @@ import com.yahya.model.Mentor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.Arrays;
