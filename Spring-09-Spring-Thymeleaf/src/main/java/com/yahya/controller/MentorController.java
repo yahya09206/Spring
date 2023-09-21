571ca8e402891bb58350d2eeb1dd1ae5511dc478
Yahya <yahyahussein91@gmail.com>
2023-09-20 20:25:47 -0700

    added list for batches

--- Spring-09-Spring-Thymeleaf/src/main/java/com/yahya/controller/MentorController.java
+++ Spring-09-Spring-Thymeleaf/src/main/java/com/yahya/controller/MentorController.java
@@ -6,6 +6,9 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.Arrays;
+import java.util.List;
+
 @Controller
 @RequestMapping("/mentor")
 public class MentorController {
@@ -13,6 +16,10 @@ public class MentorController {
     @GetMapping("/register")
     public String showForm(Model model){
         model.addAttribute("mentor", new Mentor());
+
+        List<String> batchList = Arrays.asList("JD1", "JD2", "JD3");
+        model.addAttribute("batchList", batchList);
+
         return "mentor/mentor-register";
     }
 }
