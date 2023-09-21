773098a72693c64e84f2d9cb05ac796ba143db0f
Yahya <yahyahussein91@gmail.com>
2023-09-20 20:05:05 -0700

    switched to method mapping

--- Spring-09-Spring-Thymeleaf/src/main/java/com/yahya/controller/StudentController.java
+++ Spring-09-Spring-Thymeleaf/src/main/java/com/yahya/controller/StudentController.java
@@ -3,6 +3,7 @@ package com.yahya.controller;
 import com.yahya.bootstrap.DataGenerator;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -10,14 +11,16 @@ import org.springframework.web.bind.annotation.RequestParam;
 @RequestMapping("/student")
 public class StudentController {
 
-    @RequestMapping("/register")
+    //@RequestMapping("/register")
+    @GetMapping("/register")
     public String register(Model model){
 
         model.addAttribute("students", DataGenerator.createStudent());
         return "student/register";
     }
 
-    @RequestMapping("/welcome")
+    //@RequestMapping("/welcome")
+    @GetMapping("/welcome")
     public String info(){
         return "student/welcome";
     }
