e874ef39bbde054ec55fa5b48997d6aa70343ca5
Yahya <yahyahussein91@gmail.com>
2023-09-20 20:17:33 -0700

    set up new mentor object to bring to form view

---
+++ Spring-09-Spring-Thymeleaf/src/main/java/com/yahya/controller/MentorController.java
@@ -0,0 +1,18 @@
+package com.yahya.controller;
+
+import com.yahya.model.Mentor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/mentor")
+public class MentorController {
+
+    @GetMapping("/register")
+    public String showForm(Model model){
+        model.addAttribute("mentor", new Mentor());
+        return "mentor/mentor-register";
+    }
+}
